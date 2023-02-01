feee16b36fa915e495e8e860b40c10fb42250d14
zhouhuajian <zhouhuajian@company.com>
2023-02-01 15:35:00 +0800

    初步开发在线词典用户界面

--- src/main/java/online/dictionary/OnlineDctionaryFactory.java
+++ src/main/java/online/dictionary/OnlineDctionaryFactory.java
@@ -13,7 +13,20 @@ public class OnlineDctionaryFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ContentManager contentManager = toolWindow.getContentManager();
-        Content content = contentManager.getFactory().createContent(new JLabel("label123"), "", true);
+        Content content = contentManager.getFactory().createContent(new OnlineDictionaryPanel(),
+                                                                    "",
+                                                                    true);
+        // Content content = contentManager.getFactory().createContent(new JLabel("label123"),
+        //                                                             "123",
+        //                                                             true);
+        // Content content2 = contentManager.getFactory().createContent(new JLabel("label456"),
+        //                                                             "456",
+        //                                                             false);
+        // Content content3 = contentManager.getFactory().createContent(new JLabel("label789"),
+        //                                                             "789",
+        //                                                             true);
         contentManager.addContent(content);
+        // contentManager.addContent(content2);
+        // contentManager.addContent(content3);
     }
 }
---
+++ src/main/java/online/dictionary/OnlineDictionaryPanel.java
@@ -0,0 +1,10 @@
+package online.dictionary;
+
+import javax.swing.*;
+
+public class OnlineDictionaryPanel extends JPanel {
+    public OnlineDictionaryPanel() {
+        // 表单
+
+    }
+}
