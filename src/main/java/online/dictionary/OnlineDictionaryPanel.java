1f6a15bb9a9b31f85379c2dbac580e28a5eb805c
zhouhuajian <zhouhuajian@company.com>
2023-02-06 13:18:59 +0800

    开发单词查询功能

--- src/main/java/online/dictionary/OnlineDictionaryPanel.java
+++ src/main/java/online/dictionary/OnlineDictionaryPanel.java
@@ -2,6 +2,7 @@ package online.dictionary;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class OnlineDictionaryPanel extends JPanel {
     public OnlineDictionaryPanel() {
@@ -14,11 +15,30 @@ public class OnlineDictionaryPanel extends JPanel {
         formPanel.add(lookUpButton, BorderLayout.EAST);
         // 查询结果
         JLabel resultLabel = new JLabel();
+        resultLabel.setHorizontalAlignment(JLabel.LEFT);
+        resultLabel.setVerticalAlignment(JLabel.TOP);
+        resultLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         // 整体布局
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setLayout(new BorderLayout());
         add(formPanel, BorderLayout.NORTH);
         add(resultLabel, BorderLayout.CENTER);
 
-        resultLabel.setText("123");
+        // resultLabel.setText("123");
+        lookUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultLabel.setText("这是" + wordTextField.getText() + "单词的查询结果");
+            }
+        });
+
+        wordTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    lookUpButton.doClick();
+                }
+            }
+        });
     }
 }
