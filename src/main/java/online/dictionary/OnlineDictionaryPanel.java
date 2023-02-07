4514e14cf5ae37e518bcddfb94c553c3ebdbe0ba
zhouhuajian <zhouhuajian@company.com>
2023-02-07 17:08:38 +0800

    使用爬虫实现单词查询

--- src/main/java/online/dictionary/OnlineDictionaryPanel.java
+++ src/main/java/online/dictionary/OnlineDictionaryPanel.java
@@ -1,8 +1,12 @@
 package online.dictionary;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 public class OnlineDictionaryPanel extends JPanel {
     public OnlineDictionaryPanel() {
@@ -28,7 +32,18 @@ public class OnlineDictionaryPanel extends JPanel {
         lookUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                resultLabel.setText("这是" + wordTextField.getText() + "单词的查询结果");
+                // resultLabel.setText("这是" + wordTextField.getText() + "单词的查询结果");
+                String word = wordTextField.getText();
+                String result;
+                try {
+                    Document document = Jsoup.connect("https://cn.bing.com/dict/search?q=" + word).get();
+                    String part1 = document.select("div.qdef div.hd_area").first().toString();
+                    String part2 = document.select("div.qdef ul").first().toString();
+                    result = part1 + part2;
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                resultLabel.setText("<html>" + result + "</html>");
             }
         });
 
